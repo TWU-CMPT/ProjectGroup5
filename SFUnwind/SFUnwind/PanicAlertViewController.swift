cc80fcd0f6d839d2f7f51f36c9b74589bf7755fa
Adam B <biskit@As-Mac.local>
2017-02-28 16:40:18 -0800

    Add labels to each screen to differentiate each feature

---
+++ SFUnwind/SFUnwind/PanicAlertViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PanicAlertViewController.swift
+//  SFUnwind
+//
+//  Created by A B on 2017-02-28.
+//  Copyright © 2017 CMPT 276 - Group 5. All rights reserved.
+//
+
+import UIKit
+
+class PanicAlertViewController: UIPageViewController{
+    
+}
