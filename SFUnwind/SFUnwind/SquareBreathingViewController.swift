111426f02ba7c38fe2d8525c2d53b61ad41688b5
berkeliboz <berke@berkes-Mac.local>
2017-03-02 11:46:27 -0800

    Main button coded
    For some reason it is buggy yet, will be fixed

--- SFUnwind/SFUnwind/SquareBreathingViewController.swift
+++ SFUnwind/SFUnwind/SquareBreathingViewController.swift
@@ -12,5 +12,27 @@ import UIKit
 // This is the main view controller for the SFUnwind "Square Breathing" feature
 // Primary programmer: Berke
 class SquareBreathingViewController: UIViewController{
+ 
+    @IBOutlet weak var localTimer: UILabel!
+    var sessionTime = Timer()            //NSTimer will be used
+    var sessionTimeCounter = 0         //Session time added (Will be formatted to XX:XX)
+
+
+
     
+    func sessionTimeManager(){                      //This func is called by selector of restartButton every sec
+    sessionTimeCounter += 1
+    localTimer.text = String(sessionTimeCounter)
+
+    }
+
+    @IBAction func restartButton(_ sender: UIButton) {
+        
+        sessionTime = Timer.init(timeInterval: 1, target: self, selector: #selector(SquareBreathingViewController.sessionTimeManager), userInfo: nil, repeats: true)
+    }
+    
+    
+    
+
+   
 }
