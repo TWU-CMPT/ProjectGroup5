d3258d3be5cc88fe1a30d1b73351b6a356c07a4b
zgkobe <zgkobezg@gmail.com>
2017-03-03 02:32:29 -0800

    45% done
    but theres few bugs, if i can solve it, the rest is not a problem, can
    do it within a day

--- SFUnwind/SFUnwind/PositiveAffirmationViewController.swift
+++ SFUnwind/SFUnwind/PositiveAffirmationViewController.swift
@@ -13,5 +13,49 @@ import UIKit
 // Primary programmer: Joseph
 class PositiveAffirmationViewController: UIViewController{
     
+    
+    @IBAction func Create(_ sender: AnyObject) {
+        createAlert()
+    }
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    func createAlert (){
+        let alert = UIAlertController(title: "Enter Mantra", message: "Write down the mantra you like", preferredStyle: UIAlertControllerStyle.alert)
+        
+        alert.addTextField { (textField) in textField.placeholder = "Enter here"
+        }
+        
+
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { (action) in alert.dismiss(animated: true, completion: nil)
+            }))
+        let textField = alert.textFields![0]
+        print("Text field: \(textField.text)")
+        
+        // fkfkfkfkfkfk
+        //alert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { (UIAlertAction) in print(String(enterdtext))
+        var enteredText:String
+        let action = UIAlertAction(title: "Submit", style: UIAlertActionStyle.default, handler: {[weak self]
+                                    (paramAction:UIAlertAction!) in
+                                    if var textFields = alert.textFields{
+                                        var theTextFields = textFields as [UITextField]
+                                        enteredText = theTextFields[0].text!
+                                        self?.Label.text = String(describing: enteredText)
+                                    };print(String(enteredText))
+            })
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()// do any additional setup after loading the view, typically from nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()// dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
