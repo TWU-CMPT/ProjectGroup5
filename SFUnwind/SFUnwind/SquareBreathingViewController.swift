52db4ca551bd14f50723a9c58ae00ba2e4e89b2d
berkeliboz <berke@berkes-Mac.local>
2017-03-03 17:54:59 -0800

    Restart Button comments added
    Known issues and Missing Features list added

--- SFUnwind/SFUnwind/SquareBreathingViewController.swift
+++ SFUnwind/SFUnwind/SquareBreathingViewController.swift
@@ -14,19 +14,22 @@ import UIKit
 class SquareBreathingViewController: UIViewController{
  
     @IBOutlet weak var sessionTimer: UILabel!
+    
+    
     var sessionTimeSeconds = 0
     var sessionTimeMinute = 5
     var sessionTracker = Timer()
-    var sesssionTrackerActive: Bool = false
+    var sesssionTrackerActive: Bool = false             //A boolean statement is used to keep track of the state of RE/START button. sesssionTrackerActive acts like On/Off button
+    
     
     
     func sessionTimeManager(){
         
         sessionTimeSeconds-=1                                               //Decrement Seconds
         sessionTimer.text = "0" + String(sessionTimeMinute) + ":" + String(sessionTimeSeconds) ///Print time
-        if(sessionTimeSeconds < 10){
+        if(sessionTimeSeconds < 10){                                        //Keeps XX:XX format
             sessionTimer.text = "0" + String(sessionTimeMinute) + ":0" + String(sessionTimeSeconds)
-            if(sessionTimeSeconds <= -1){
+            if(sessionTimeSeconds <= -1){                                   //Detects if timer is finished
                 sessionTracker.invalidate()
                 sessionTimer.text = "FINISHED"
             }
@@ -40,12 +43,12 @@ class SquareBreathingViewController: UIViewController{
     
     @IBAction func restartButton(_ sender: Any) {
     sessionTracker = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(SquareBreathingViewController.sessionTimeManager), userInfo: nil, repeats: true)
-        if(sessionTimeSeconds == 0 && sessionTimeMinute != 0){
-            sessionTimeMinute-=1
-            sessionTimeSeconds = 60
+        if(sessionTimeSeconds == 0 && sessionTimeMinute != 0){                 //If Remaining mins != 0
+            sessionTimeMinute-=1                                               //Decrement Minutes
+            sessionTimeSeconds = 60                                            //Decrement Seconds
         }
     
-        else{                                               //If pressed again
+        else{                                               //If RESETED
             sessionTracker.invalidate()                         //Stop
             sessionTimeSeconds = 60                             //Reset
             sessionTimeMinute = 4                               //Reset
@@ -55,3 +58,21 @@ class SquareBreathingViewController: UIViewController{
         }
     }
 }
+
+//Known issues
+//
+//
+//      - Timer doesn't stop when page is changed.
+//      - There is a slight latency when RE/START button is pressed on VM, must be tested in an actual iPhone.
+//
+//
+//
+
+//Missing Features
+//
+//      - Main Timer is not implemented yet
+//      - Main Timer file I/O not implemented yet
+//
+//
+//
+//
