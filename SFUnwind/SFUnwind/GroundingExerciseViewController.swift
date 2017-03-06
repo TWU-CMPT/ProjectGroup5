74bebfce4c68c7975ada82036f1c93299e1c6f02
dm <dm@dms-iMac.local>
2017-03-06 00:40:55 -0800

    Overlay working, with switching text.

--- SFUnwind/SFUnwind/GroundingExerciseViewController.swift
+++ SFUnwind/SFUnwind/GroundingExerciseViewController.swift
@@ -10,21 +10,36 @@
 
 import UIKit
 
+
+
 class GroundingFeatureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     
     @IBOutlet weak var pickedImaged: UIImageView! // Image UI we have on screen.
     
+    var strings = ["[SEE]", "[TOUCH]", "[HEAR]"]
+    var stringCount = 0
+    
+    @IBOutlet weak var theOverlay: UIImageView!
+
+    @IBOutlet weak var theLabel: UILabel!
+    
     @IBAction func cameraButtonAction(_ sender: UIButton) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
             let imagePicker = UIImagePickerController()
+            let zlabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+            zlabel.center = CGPoint(x: 160,y: 284)
+            zlabel.textAlignment = .center
+            zlabel.text = strings[stringCount]
+            stringCount+=1
+            stringCount = stringCount % 3
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
             imagePicker.allowsEditing = false
             imagePicker.cameraCaptureMode = .photo
             imagePicker.showsCameraControls = true
             imagePicker.modalPresentationStyle = .fullScreen
-            self.present(imagePicker, animated: true, completion: nil)
+            self.present(imagePicker, animated: true, completion: {imagePicker.cameraOverlayView = zlabel})
         } else {
             let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera", preferredStyle: .alert)
             let okaAction = UIAlertAction(title: "OK", style: .default, handler: nil)
@@ -32,6 +47,9 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
             present(alertVC, animated: true, completion: nil)
         }
     }
+    
+
+    
     
     
     @IBAction func photoLibraryAction(_ sender: UIButton) {
@@ -46,10 +64,10 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
     
     @IBAction func saveAction(_ sender: UIButton) {
         if pickedImaged.image != nil {
-        let imageData = UIImageJPEGRepresentation(pickedImaged.image!, 0.6)
-        let compressedJPEGImage = UIImage(data: imageData!)
-        UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
-        saveNotice()
+            let imageData = UIImageJPEGRepresentation(pickedImaged.image!, 0.6)
+            let compressedJPEGImage = UIImage(data: imageData!)
+            UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil)
+            saveNotice()
         }
         else {
             let alertController = UIAlertController(title: "Select Image!", message: "Image not found!", preferredStyle: .alert)
