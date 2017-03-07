a7dc66b4a8f6a97a4323ec86b70a2de49f704b94
dm <dm@dms-iMac.local>
2017-03-06 19:07:49 -0800

    For Visual Presentation Version 1

--- SFUnwind/SFUnwind/GroundingExerciseViewController.swift
+++ SFUnwind/SFUnwind/GroundingExerciseViewController.swift
@@ -17,8 +17,15 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
     
     @IBOutlet weak var pickedImaged: UIImageView! // Image UI we have on screen. Get's replaced by the image itself at some point.
     
+    @IBOutlet weak var mainLabel: UILabel!
+    
     var strings = ["[SEE]", "[TOUCH]", "[HEAR]"] // Scroll through these.
     var stringCount = 0 // Current index of strings
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainLabel.text = strings[stringCount]
+    }
     
     @IBAction func cameraButtonAction(_ sender: UIButton) { // Capture Button
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
@@ -27,8 +34,7 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
             zlabel.center = CGPoint(x: 160,y: 284) // Specify stuff about overlay
             zlabel.textAlignment = .center
             zlabel.text = strings[stringCount]
-            stringCount+=1 // Move index
-            stringCount = stringCount % 3
+            
             imagePicker.delegate = self
             imagePicker.sourceType = UIImagePickerControllerSourceType.camera; // Source becomes camera
             imagePicker.allowsEditing = false // Do not edit photos that we capture
@@ -44,46 +50,53 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
         }
     }
     
-
     
     
     
-    @IBAction func photoLibraryAction(_ sender: UIButton) { // This is for testing saving without camera - just select from default found in emulators
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){ // if there's a photo library.
-            let imagePicker = UIImagePickerController() // The image picker will pick the image
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary; // Set it to photo library selection
-            imagePicker.allowsEditing = true // Allow user to edit, why not
-            self.present(imagePicker, animated: true, completion: nil) // No overlay this time.
-        }
-    }
-    
-    @IBAction func saveAction(_ sender: UIButton) { // This is when user saves
-        if pickedImaged.image != nil { // In the case that the user does not actually have anything captured.
-            let imageData = UIImageJPEGRepresentation(pickedImaged.image!, 0.6) // Compression and data for insert into phone
-            let compressedJPEGImage = UIImage(data: imageData!)
-            UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil) // Saves to photo album
-            saveNotice() // Alerts user that there's been a save.
-        }
-        else { // If there's nothing saved, give alert.
-            let alertController = UIAlertController(title: "Select Image!", message: "Image not found!", preferredStyle: .alert)
-            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertController.addAction(defaultAction)
-            present(alertController, animated: true, completion: nil) // Present alert
-        }
-    }
-    
+    /*
+     // REMOVE BEFORE POST
+     @IBAction func photoLibraryAction(_ sender: UIButton) { // This is for testing saving without camera - just select from default found in emulators
+     if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){ // if there's a photo library.
+     let imagePicker = UIImagePickerController() // The image picker will pick the image
+     imagePicker.delegate = self
+     imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary; // Set it to photo library selection
+     imagePicker.allowsEditing = true // Allow user to edit, why not
+     self.present(imagePicker, animated: true, completion: nil) // No overlay this time.
+     }
+     }
+     
+     
+     @IBAction func saveAction(_ sender: UIButton) { // This is when user saves
+     if pickedImaged.image != nil { // In the case that the user does not actually have anything captured.
+     let imageData = UIImageJPEGRepresentation(pickedImaged.image!, 0.6) // Compression and data for insert into phone
+     let compressedJPEGImage = UIImage(data: imageData!)
+     UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil) // Saves to photo album
+     saveNotice() // Alerts user that there's been a save.
+     }
+     else { // If there's nothing saved, give alert.
+     let alertController = UIAlertController(title: "Select Image!", message: "Image not found!", preferredStyle: .alert)
+     let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+     alertController.addAction(defaultAction)
+     present(alertController, animated: true, completion: nil) // Present alert
+     }
+     }
+     
+     func saveNotice(){ // Gives notice on succesful save
+     let alertController = UIAlertController(title: "Image Saved!", message: "Your picture was succesfully saved!", preferredStyle: .alert)
+     let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+     alertController.addAction(defaultAction)
+     present(alertController, animated: true, completion: nil)
+     }
+     */
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) { // This func sets current image stored on pickedImage to be made image.
         // Apparently called automatically be the camera function.
-        pickedImaged.image = image
+        //pickedImaged.image = image
+        stringCount+=1 // Move index
+        stringCount = stringCount % 3
+        mainLabel.text = strings[stringCount]
         self.dismiss(animated: true, completion: nil);
     }
     
-    func saveNotice(){ // Gives notice on succesful save
-        let alertController = UIAlertController(title: "Image Saved!", message: "Your picture was succesfully saved!", preferredStyle: .alert)
-        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(defaultAction)
-        present(alertController, animated: true, completion: nil)
-    }
+    
     
 }
