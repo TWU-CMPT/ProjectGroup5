7dc737a608eff4511bf15ca7255d31cd0c950af3
dm <dm@dms-iMac.local>
2017-03-08 02:22:13 -0800

    Tests created for everything. Test for alert still in progress.

--- SFUnwind/SFUnwind/GroundingExerciseViewController.swift
+++ SFUnwind/SFUnwind/GroundingExerciseViewController.swift
@@ -23,13 +23,17 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
     
     var strings = ["[SEE]", "[TOUCH]", "[HEAR]"] // Scroll through these.
     var stringCount = 0 // Current index of strings
+    let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera", preferredStyle: .alert) // Create alerts
+    let okaAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         captureButton.layer.cornerRadius=20
         mainLabel.text = strings[stringCount]
+        alertVC.addAction(okaAction)
     }
-    
+    // This
     @IBAction func cameraButtonAction(_ sender: UIButton) { // Capture Button
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
             let imagePicker = UIImagePickerController() // New picker controller: image picker
@@ -46,54 +50,12 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
             imagePicker.modalPresentationStyle = .fullScreen // Redudant, kinda
             self.present(imagePicker, animated: true, completion: {imagePicker.cameraOverlayView = zlabel}) // Activate the camera!
         } else { // You get this if you do not have a camera.
-            let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera", preferredStyle: .alert) // Create alerts
-            let okaAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-            alertVC.addAction(okaAction)
+            
             present(alertVC, animated: true, completion: nil) //Present alert
         }
     }
-    
-    
-    
-    
-    /*
-     // REMOVE BEFORE POST
-     @IBAction func photoLibraryAction(_ sender: UIButton) { // This is for testing saving without camera - just select from default found in emulators
-     if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary){ // if there's a photo library.
-     let imagePicker = UIImagePickerController() // The image picker will pick the image
-     imagePicker.delegate = self
-     imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary; // Set it to photo library selection
-     imagePicker.allowsEditing = true // Allow user to edit, why not
-     self.present(imagePicker, animated: true, completion: nil) // No overlay this time.
-     }
-     }
-     
-     
-     @IBAction func saveAction(_ sender: UIButton) { // This is when user saves
-     if pickedImaged.image != nil { // In the case that the user does not actually have anything captured.
-     let imageData = UIImageJPEGRepresentation(pickedImaged.image!, 0.6) // Compression and data for insert into phone
-     let compressedJPEGImage = UIImage(data: imageData!)
-     UIImageWriteToSavedPhotosAlbum(compressedJPEGImage!, nil, nil, nil) // Saves to photo album
-     saveNotice() // Alerts user that there's been a save.
-     }
-     else { // If there's nothing saved, give alert.
-     let alertController = UIAlertController(title: "Select Image!", message: "Image not found!", preferredStyle: .alert)
-     let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-     alertController.addAction(defaultAction)
-     present(alertController, animated: true, completion: nil) // Present alert
-     }
-     }
-     
-     func saveNotice(){ // Gives notice on succesful save
-     let alertController = UIAlertController(title: "Image Saved!", message: "Your picture was succesfully saved!", preferredStyle: .alert)
-     let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-     alertController.addAction(defaultAction)
-     present(alertController, animated: true, completion: nil)
-     }
-     */
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) { // This func sets current image stored on pickedImage to be made image.
-        // Apparently called automatically be the camera function.
-        //pickedImaged.image = image
+    // This function sets current goal displayed on main screen to be correct.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
         stringCount+=1 // Move index
         stringCount = stringCount % 3
         mainLabel.text = strings[stringCount]
--- SFUnwind/SFUnwindTests/GroundingExerciseViewControllerTests.swift
+++ SFUnwind/SFUnwindTests/GroundingExerciseViewControllerTests.swift
@@ -12,14 +12,58 @@ import XCTest
 
 class GroundingFeatureViewControllerTests: XCTestCase {
     
+    var theSFUnwindPageViewController: SFUnwindPageViewController? // Add an instance of the panic alert view controller object to this test class
+    
+    var theGroundingExerciseViewController: GroundingFeatureViewController? // Add an instance of the panic alert view controller object to this test class
+    
     override func setUp() {
         super.setUp()
+        
+        let theStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main) // Create a storyboard object to use for our tests
+        
+        theSFUnwindPageViewController = theStoryboard.instantiateInitialViewController() as! SFUnwindPageViewController? // Create a SFUnwindPageViewController object for our tests
+        theGroundingExerciseViewController = theSFUnwindPageViewController?.theViewControllers[1] as! GroundingFeatureViewController? // Create a GroundingFeatureViewController object for our tests
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    
+    // Tests:
+    //*****************
+    
+    func testMainLabelsShowingProperly() {
+        XCTAssert(theGroundingExerciseViewController?.mainLabel == nil)
+        let _ = theGroundingExerciseViewController?.view
+        XCTAssert(theGroundingExerciseViewController?.mainLabel.text != nil)
+        XCTAssert(theGroundingExerciseViewController?.mainLabel.text == theGroundingExerciseViewController?.strings[(theGroundingExerciseViewController?.stringCount)!])
+        XCTAssert(theGroundingExerciseViewController?.captureButton.layer.cornerRadius == 20)
+    }
+    
+    func testimagePickerController(){
+        let _ = theGroundingExerciseViewController?.view
+        let testCheckCount = theGroundingExerciseViewController?.stringCount
+        let examplePic = UIImage(named: "ImageExample")
+        let dictionary = [NSObject: AnyObject]()
+        let imagePicker = UIImagePickerController()
+        theGroundingExerciseViewController?.imagePickerController(imagePicker, didFinishPickingImage: examplePic, editingInfo : dictionary)
+        XCTAssert(testCheckCount! == (theGroundingExerciseViewController?.stringCount)! - 1)
+        XCTAssert((theGroundingExerciseViewController?.mainLabel.text)! == theGroundingExerciseViewController?.strings[testCheckCount!+1])
+    }
+    
+    func testCameraButtonAction(){
+        let _ = theGroundingExerciseViewController?.view
+        let testButton = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)){
+            theGroundingExerciseViewController?.cameraButtonAction(testButton)
+            //User must test.
+        }
+        else{
+            theGroundingExerciseViewController?.cameraButtonAction(testButton)
+        }
     }
     
 //    func testExample() {
