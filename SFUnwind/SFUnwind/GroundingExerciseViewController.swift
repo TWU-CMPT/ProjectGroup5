c3991e89d26899091a32b0b2d1ff860d7519a616
dm <dm@dms-iMac.local>
2017-03-08 04:15:01 -0800

    Fixed button linking

--- SFUnwind/SFUnwind/GroundingExerciseViewController.swift
+++ SFUnwind/SFUnwind/GroundingExerciseViewController.swift
@@ -15,53 +15,47 @@ import UIKit
 class GroundingFeatureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     
-    @IBOutlet weak var pickedImaged: UIImageView! // Image UI we have on screen. Get's replaced by the image itself at some point.
-    
-    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var goalDisplay: UILabel!
     
     @IBOutlet weak var captureButton: UIButton!
     
-    var strings = ["[SEE]", "[TOUCH]", "[HEAR]"] // Scroll through these.
-    var stringCount = 0 // Current index of strings
-    let alertVC = UIAlertController(title: "For Testing", message: "Sorry, this device has no camera", preferredStyle: .alert) // Create alerts
+    var goalString = ["[SEE]", "[TOUCH]", "[HEAR]"] // Scroll through these.
+    var goalIndex = 0 // Current index of strings
+    let theAlert = UIAlertController(title: "For Testing", message: "Sorry, this device has no camera", preferredStyle: .alert) // Create alerts
     let theOkAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         captureButton.layer.cornerRadius=20
-        mainLabel.text = strings[stringCount]
-        alertVC.addAction(theOkAction)
+        goalDisplay.text = goalString[goalIndex]
+        theAlert.addAction(theOkAction)
     }
     // This
     @IBAction func cameraButtonAction(_ sender: UIButton) { // Capture Button
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
-            let imagePicker = UIImagePickerController() // New picker controller: image picker
-            let zlabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21)) // The overlay we want
-            zlabel.center = CGPoint(x: 160,y: 284) // Specify stuff about overlay
-            zlabel.textAlignment = .center
-            zlabel.text = strings[stringCount]
+            let pickedImager = UIImagePickerController() // New picker controller: image picker
+            let overlayLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21)) // The overlay we want
+            overlayLabel.center = CGPoint(x: 160,y: 284) // Specify stuff about overlay
+            overlayLabel.textAlignment = .center
+            overlayLabel.text = goalString[goalIndex]
             
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera; // Source becomes camera
-            imagePicker.allowsEditing = false // Do not edit photos that we capture
-            imagePicker.cameraCaptureMode = .photo // To be specific, redudant
-            imagePicker.showsCameraControls = true // Redudant
-            imagePicker.modalPresentationStyle = .fullScreen // Redudant, kinda
-            self.present(imagePicker, animated: true, completion: {imagePicker.cameraOverlayView = zlabel}) // Activate the camera!
+            pickedImager.delegate = self
+            pickedImager.sourceType = UIImagePickerControllerSourceType.camera; // Source becomes camera
+            pickedImager.allowsEditing = false // Do not edit photos that we capture
+            pickedImager.cameraCaptureMode = .photo // To be specific, redudant
+            pickedImager.showsCameraControls = true // Redudant
+            pickedImager.modalPresentationStyle = .fullScreen // Redudant, kinda
+            self.present(pickedImager, animated: true, completion: {pickedImager.cameraOverlayView = overlayLabel}) // Activate the camera!
         } else { // You get this if you do not have a camera.
-            alertVC.title = "No Camera"
-            present(alertVC, animated: true, completion: nil) //Present alert
+            theAlert.title = "No Camera"
+            present(theAlert, animated: true, completion: nil) //Present alert
         }
     }
     // This function sets current goal displayed on main screen to be correct.
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        stringCount+=1 // Move index
-        stringCount = stringCount % 3
-        mainLabel.text = strings[stringCount]
+        goalIndex+=1 // Move index
+        goalIndex = goalIndex % 3
+        goalDisplay.text = goalString[goalIndex]
         self.dismiss(animated: true, completion: nil);
     }
-    
-    
-    
 }
