9aa216f5002013415a5eb6540a78fc9b2e65c092
Adam B <biskit@As-Mac.local>
2017-03-09 13:15:15 -0800

    Cleanup: Deleted unneeded files/folders. Added new files for version 2

---
+++ SFUnwind/SFUnwind/HelpViewController.swift
@@ -0,0 +1,11 @@
+//
+// HelpViewController.swift - Help view Controller for all screens
+// SFUnwind
+// Project Group 5: SFU CMPT 276
+// Primary programmer: All
+// Contributing Programmers: All
+// Known issues: 
+//
+// Note: All files in this project conform to the coding standard included in the SFUnwind HW3 Quality Assurance Documentation
+
+import Foundation
