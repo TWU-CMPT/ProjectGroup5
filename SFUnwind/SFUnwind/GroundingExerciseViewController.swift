47a3e4c8381cf8c9d26fbd5e344b769109f6f07a
dm <dm@dms-iMac.local>
2017-03-13 22:41:24 -0700

    Cool camera overlay and stuff.

--- SFUnwind/SFUnwind/GroundingExerciseViewController.swift
+++ SFUnwind/SFUnwind/GroundingExerciseViewController.swift
@@ -9,9 +9,10 @@
 // Note: All files in this project conform to the coding standard included in the SFUnwind HW3 Quality Assurance Documentation
 
 import UIKit
+import AVFoundation
 
-class GroundingFeatureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-
+class GroundingFeatureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate{
+    
     // Properties:
     //******************
     
@@ -44,11 +45,59 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
     
     // ViewDidLoad: This function is called once the GroundingExerciseViewController.swift object is first initialized.
     // This function is used to start up the UI elements, and add the alert reply as needed.
+    
+    lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let preview =  AVCaptureVideoPreviewLayer(session: self.cameraSession)
+        preview?.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        preview?.position = CGPoint(x: self.view.bounds.midX, y: self.view.bounds.midY)
+        preview?.videoGravity = AVLayerVideoGravityResizeAspect
+        return preview!
+    }()
+    
+    lazy var cameraSession: AVCaptureSession = {
+        let s = AVCaptureSession()
+        s.sessionPreset = AVCaptureSessionPresetLow
+        return s
+    }()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.layer.insertSublayer(previewLayer, at: 0)
+        
+        cameraSession.startRunning()
+    }
+    
+    var stillImageOutput = AVCaptureStillImageOutput.init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         captureButton.layer.cornerRadius=20
         goalDisplay.text = goalString[goalIndex]
         theAlert.addAction(theOkAction)
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo) as AVCaptureDevice
+            do {
+                let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+                cameraSession.beginConfiguration()
+                if (cameraSession.canAddInput(deviceInput) == true) {
+                    cameraSession.addInput(deviceInput)
+                }
+                let dataOutput = AVCaptureVideoDataOutput()
+                dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_420YpCbCr8BiPlanarFullRange as UInt32)]
+                dataOutput.alwaysDiscardsLateVideoFrames = true
+                if (cameraSession.canAddOutput(dataOutput) == true) {
+                    cameraSession.addOutput(dataOutput)
+                }
+                cameraSession.commitConfiguration()
+                let queue = DispatchQueue(label: "com.invasivecode.videoQueue")
+                dataOutput.setSampleBufferDelegate(self, queue: queue)
+            }
+            catch let error as NSError {
+                NSLog("\(error), \(error.localizedDescription)")
+            }
+            stillImageOutput.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            self.cameraSession.addOutput(stillImageOutput)
+        }
     }
     
     // cameraButtonAction: This function is called when the user presses the appropiate button.
@@ -56,32 +105,31 @@ class GroundingFeatureViewController: UIViewController, UIImagePickerControllerD
     @IBAction func cameraButtonAction(_ sender: UIButton) {
         // Test if camera exists
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
-            let pickedImager = UIImagePickerController() // New image picker controller: image picker
-            // The overlay label is to display the goal while in camera view. Also specify contents of label.
-            let overlayLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-            overlayLabel.center = CGPoint(x: 160,y: 284)
-            overlayLabel.textAlignment = .center
-            overlayLabel.text = goalString[goalIndex]
-            // Set delegate to self.
-            pickedImager.delegate = self
-            // Set source type of pickedImager to camera.
-            pickedImager.sourceType = UIImagePickerControllerSourceType.camera;
-            // We do not want to edit photos that come directly from camera, so we set this to false.
-            pickedImager.allowsEditing = false
-            // Redundancy to silence warnings.
-            pickedImager.cameraCaptureMode = .photo
-            // Redundency to minimize errors during camera capture.
-            pickedImager.showsCameraControls = true
-            // Presents the output image in fullscreen when functionality is implemented.
-            pickedImager.modalPresentationStyle = .fullScreen
-            // Presents the camera to the user in order to take photos.
-            self.present(pickedImager, animated: true, completion: {pickedImager.cameraOverlayView = overlayLabel}) // Activate the camera!
+            if let videoConnection = stillImageOutput.connection(withMediaType:AVMediaTypeVideo){
+                stillImageOutput.captureStillImageAsynchronously(from:videoConnection, completionHandler: {
+                    (sampleBuffer, error) in
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProvider.init(data: imageData as! CFData)
+                    let cgImageRef = CGImage.init(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+                    let image = UIImage.init(cgImage: cgImageRef!, scale: 1.0, orientation: .right)
+                    // do something with image
+                    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                })
+            }
         } else { // You get this if you do not have a camera.
             // For testing.
             theAlert.title = "No Camera"
             // Present alert to user.
             present(theAlert, animated: true, completion: nil) //Present alert
         }
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        // Here you collect each frame and process it
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didDrop sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
+        // Here you can count how many frames are dopped
     }
     
     
