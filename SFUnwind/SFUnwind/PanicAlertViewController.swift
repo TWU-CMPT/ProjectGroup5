c62f61d456ba0fae029bbdfc7a07c5a9a8cb9a00
dm <dm@dms-iMac.local>
2017-03-30 03:45:31 -0700

    Also added scaling. Forgot about that.

--- SFUnwind/SFUnwind/PanicAlertViewController.swift
+++ SFUnwind/SFUnwind/PanicAlertViewController.swift
@@ -3,7 +3,7 @@
 // SFUnwind
 // Project Group 5: SFU CMPT 276
 // Primary programmer: Adam Badke #301310785
-// Contributing Programmers:
+// Contributing Programmers: David Magaril #301274978
 // Known issues: 
 //
 // Note: All files in this project conform to the coding standard included in the SFUnwind HW3 Quality Assurance Documentation
